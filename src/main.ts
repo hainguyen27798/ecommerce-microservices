b206b20e7eaa357943e20fc37fd947360767f0ca
Harry Nguyen <hainguyen27798@gmail.com>
2024-04-22 18:21:21 +0700

    setup config and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,48 @@
+import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+
+import { Configuration } from '@/config/configuration';
+import { ENV_MODE } from '@/constants';
+import { LoggerServerHelper } from '@/helpers';
+import { setupSwagger } from '@/setup-swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+
+    app.enableCors({
+        origin: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    });
+
+    app.enableVersioning({
+        type: VersioningType.URI,
+        defaultVersion: '1',
+    });
+
+    // init logger
+    LoggerServerHelper.init();
+    app.useLogger(LoggerServerHelper.instance);
+
+    // apply http logger
+    app.use(LoggerServerHelper.morganMiddleware);
+
+    // validate input before jump into controller
+    app.useGlobalPipes(new ValidationPipe());
+
+    if (Configuration.instance.env !== ENV_MODE.PRO) {
+        setupSwagger(app);
+    }
+
+    app.enableShutdownHooks();
+
+    await app.listen(Configuration.instance.port);
 }
-bootstrap();
+
+bootstrap().then(() => {
+    LoggerServerHelper.log(`Server running at: http://localhost:${Configuration.instance.port}`);
+}).catch(reason => {
+    LoggerServerHelper.error(`Server occurred error: ${reason}`);
+});
---
+++ src/types/config.d.ts
@@ -0,0 +1,14 @@
+import { ENV_MODE } from '@/constants';
+
+export type TConfig = {
+    env: ENV_MODE;
+    port: number;
+    mongo: {
+        port: number;
+        host: string;
+        username: string;
+        password: string;
+        databaseName: string;
+        authSource: string;
+    };
+};
