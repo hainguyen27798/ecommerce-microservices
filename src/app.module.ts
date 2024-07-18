c0867b2d68239883d865ab32b93b89b9e8150d27
Harry Nguyen <70564906+hainguyen27798@users.noreply.github.com>
2024-07-18 23:12:17 +0700

    setup and connect redis (#10)

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,6 +12,8 @@ import { ProductModule } from '@/modules/product/product.module';
 import { TokenModule } from '@/modules/token/token.module';
 import { UserModule } from '@/modules/user/user.module';
 
+import { RedisModule } from './modules/redis/redis.module';
+
 @Module({
     imports: [
         ConfigModule.forRoot({
@@ -19,6 +21,7 @@ import { UserModule } from '@/modules/user/user.module';
             isGlobal: true,
             load: [Configuration.init],
         }),
+        RedisModule,
         DatabaseModule,
         UserModule,
         TokenModule,
