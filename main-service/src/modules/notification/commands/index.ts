76e308c0fc4edc678c6712a577a7403abdb4ef9b
harry Nguyen <hainguyen27798@gmail.com>
2024-08-10 17:45:41 +0700

    delete notification from main project

--- main-service/src/modules/notification/commands/index.ts
+++
@@ -1,5 +0,0 @@
-import { PushNotificationToSystemHandler } from '@/modules/notification/commands/push-notification-to-system.command';
-
-export * from './push-notification-to-system.command';
-
-export const handlers = [PushNotificationToSystemHandler];
--- main-service/src/modules/notification/types/notification-payload.type.ts
+++
@@ -1,9 +0,0 @@
-import { NotificationType } from '@/modules/notification/constants';
-
-export type NotificationPayloadType = {
-    type: NotificationType;
-    senderId: string;
-    receiverId: string;
-    content: string;
-    options?: unknown;
-};
